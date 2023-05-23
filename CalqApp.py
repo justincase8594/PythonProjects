71b7ffc6ea2989c3078f8d6e09f85d67eb0db5de
Justin Rodriguez <justincase8594@gmail.com>
2023-05-22 21:55:18 -0500

    the start of something maybe terrible...

---
+++ CalqApp.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+# Function for function button clicks
+
+window = tk.Tk()
+window.title("CalQlator")
+buttons = [
+    "C", "+/-", "%", "/",
+    "7", "8", "9", "X",
+    "4", "5", "6", "-",
+    "1", "2", "3", "+",
+    "0", ".", "=" 
+]
+
+window.mainloop()
