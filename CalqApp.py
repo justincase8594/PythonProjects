4f2ff209fb5e33e860326f7df79730d4b3680e39
Justin Rodriguez <justincase8594@gmail.com>
2023-05-25 19:46:45 -0500

    functional calculator, and working on 1.1 version

--- CalqApp.py
+++ CalqApp.py
@@ -1,15 +1,61 @@
+# make a calculator app that does all basic functions
+# this will not do anything besides basic arithmatic
+# that would take forever
+#1> make GUI and buttons (buttons not functional)
+#2> make each button functional
+#3> make functionality between butons useful to creating
+#an actual calculator result
+#4> make it look sleek
+
 import tkinter as tk
 
 # Function for function button clicks
 
+def button_click(number):
+    current_text = screenLabel['text']
+    screenLabel["text"] = current_text + str(number)
+
+def clear_screen():
+    screenLabel["text"] = ""
+
+def calculate_result():
+    expression = screenLabel["text"]
+    try:
+        result = eval(expression)
+        screenLabel["text"] = str(result)
+    except Exception as e:
+        screenLabel["text"] = "Error"
+
+#create ui window
 window = tk.Tk()
 window.title("CalQlator")
-buttons = [
-    "C", "+/-", "%", "/",
-    "7", "8", "9", "X",
-    "4", "5", "6", "-",
-    "1", "2", "3", "+",
-    "0", ".", "=" 
-]
+screenLabel = tk.Label(window, text="", anchor="e", relief="ridge", width=30)
+screenLabel.grid(row=0, column=0, columnspan=4, padx=5, pady=5, sticky="nsew")
+#less repetative code for simple buttons, dictionary will make it easier to format
+buttons = {
+    "C": (1, 0), "+/-": (1, 1), "%": (1, 2), "/": (1,3),
+    "7": (2, 0), "8": (2, 1), "9": (2, 2), "X": (2, 3),
+    "4": (3, 0), "5": (3, 1), "6": (3, 2), "-": (3, 3),
+    " ": (4, 0), "0": (4, 1), ".": (4, 2), "=": (4, 3)
+}
+# using the dictionary with row and columns to set up button on grid
+#**don't forget to add the screen later
+for button_text, (row, column) in buttons.items():
+    if button_text == "=":
+        button_widget = tk.Button(window, text=button_text, width=5, command=calculate_result)
+    elif button_text == "C":
+        button_widget = tk.Button(window, text=button_text, width=5, command=clear_screen)
+    else:
+        button_widget = tk.Button(window, text=button_text, width=5, command=lambda text=button_text: button_click(text))
+    button_widget.grid(row = row, column = column, padx=3, pady=5, sticky="nsew")
+#buttons don't move when i expand calq app
+#need to integrate some type of auto adjust for buttons
+#this move the buttons and scree but does not change the size
+for i in range(4):
+    window.grid_columnconfigure(i, weight=1)
+for i in range(5):
+    window.grid_rowconfigure(0, weight=1)
+
+
 
 window.mainloop()
---
+++ CalqApp1.1.py
@@ -0,0 +1,107 @@
+import tkinter as tk
+import math
+
+# Function for number button clicks
+def button_click(number):
+    
+    current_text = screenLabel['text']
+    screenLabel["text"] = current_text + str(number)
+
+# Function to clear the screen
+def clear_screen():
+    screenLabel["text"] = ""
+
+# Function to calculate the result
+def calculate_result(event=None):
+    expression = screenLabel["text"]
+    try:
+        result = eval(expression)
+        screenLabel["text"] = str(result)
+    except Exception as e:
+        screenLabel["text"] = "Error"
+f"""def calculate_result():
+    expression = screenLabel["text"]
+    try:
+        result = eval_expression(expression)
+        screenLabel["text"] = str(result)
+    except Exception as e:
+        screenLabel["text"] = "Error"""
+
+# Function to evaluate the expression
+def eval_expression(expression):
+    operators = {
+        "+": lambda x, y: x + y,
+        "-": lambda x, y: x - y,
+        "*": lambda x, y: x * y,
+        "/": lambda x, y: x / y,
+    }
+
+    tokens = expression.split()
+    stack = []
+
+    for token in tokens:
+        if token.isdigit():
+            stack.append(float(token))
+        elif token in operators:
+            if len(stack) < 2:
+                raise ValueError("Invalid expression")
+            y = stack.pop()
+            x = stack.pop()
+            result = operators[token](x, y)
+            stack.append(result)
+        else:
+            raise ValueError("Invalid expression")
+
+    if len(stack) != 1:
+        raise ValueError("Invalid expression")
+
+    return stack[0]
+
+# Create UI window
+window = tk.Tk()
+window.title("CalQlator")
+
+# Create screen label
+screenLabel = tk.Label(window, text="", anchor="e", relief="ridge", width=30)
+screenLabel.grid(row=0, column=0, columnspan=4, padx=5, pady=5, sticky="nsew")
+
+# Define button positions in a dictionary
+buttons = {
+    "C": (1, 0), "": (1, 1), "*": (1, 2), "/": (1, 3),
+    "7": (2, 0), "8": (2, 1), "9": (2, 2), "+": (2, 3),
+    "4": (3, 0), "5": (3, 1), "6": (3, 2), "-": (3, 3),
+    " ": (4, 0), "0": (4, 1), ".": (4, 2), "=": (4, 3)
+}
+
+# Create buttons and assign functions to them
+for button_text, (row, column) in buttons.items():
+    if button_text == "=":
+        button_widget = tk.Button(window, text=button_text, width=5, command=calculate_result)
+    elif button_text == "C":
+        button_widget = tk.Button(window, text=button_text, width=5, command=clear_screen)
+    elif button_text == "/":
+        button_widget = tk.Button(window, text=button_text, width=5, command=lambda: perform_operation("/"))
+    elif button_text == "*":
+        button_widget = tk.Button(window, text=button_text, width=5, command=lambda: perform_operation("*"))
+    else:
+        button_widget = tk.Button(window, text=button_text, width=5, command=lambda text=button_text: button_click(text))
+    button_widget.grid(row=row, column=column, padx=3, pady=5, sticky="nsew")
+
+# Configure column and row weights for dynamic button resizing
+for i in range(4):
+    window.grid_columnconfigure(i, weight=1)
+for i in range(5):
+    window.grid_rowconfigure(0, weight=1)
+
+# Function to perform the operation
+def perform_operation(operator):
+    current_text = screenLabel["text"]
+    if current_text and not current_text.endswith(operator):
+        screenLabel["text"] = current_text + " " + operator
+    elif operator:
+        screenLabel["text"] = operator
+
+
+
+# Start the GUI main loop
+window.mainloop()
